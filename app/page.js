046a14eaa5b98c20c0f93d500c962e650ba33dc0
Syahrul-123 <138555351+Syahrul-123@users.noreply.github.com>
2024-08-08 07:57:43 +0000

    chapter2 selesai

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Page() {
       <div className="flex items-end h-20 p-4 bg-blue-500 rounded-lg shrink-0 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
+      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
           <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
