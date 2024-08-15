483e5ff93bf4b86db376efd266501d902d10d62b
syahrul suhardi <105841100721@student.unismuh.ac.id>
2024-08-15 00:03:48 -0700

    chapter 7 baru baru

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,6 +1,5 @@
 import { sql } from '@vercel/postgres';
 import { formatCurrency } from './utils';
-// /app/dashboard/page.jsx
 import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
